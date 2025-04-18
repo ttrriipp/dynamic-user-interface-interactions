f6abf59ce9a05cbe65fcc0bb33c0a3639d8dfe2a
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-04-18 16:40:51 +0800

    ignore webpack configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,18 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-]);
+export default defineConfig(
+  [
+    {
+      files: ['**/*.{js,mjs,cjs}'],
+      plugins: { js },
+      extends: ['js/recommended'],
+    },
+    {
+      files: ['**/*.{js,mjs,cjs}'],
+      languageOptions: { globals: globals.browser },
+    },
+  ],
+  globalIgnores(['webpack.common.js', 'webpack.dev.js', 'webpack.prod.js'])
+);
